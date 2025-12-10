9c41174489f559bd1dfce2bea99dc30874f54a35
cosmos-junior <joachimnevilleodhiambo@gmail.com>
2025-12-10 09:56:44 +0300

    updated

--- hospitalProject/urls.py
+++ hospitalProject/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('hospital.urls')),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 urlpatterns += staticfiles_urlpatterns()
